6313c1ec90cefca33819b7766868d7e0c657d818
Malekian <malekian@protonmail.com>
2026-06-12 20:58:10 +0400

    - Added database settings - Added alembic

--- src/settings.py
+++ src/settings.py
@@ -1,10 +1,24 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     bot_token: str
 
-    model_config = dict(
+    db_host: str = "localhost"
+    db_port: int = 5432
+    db_user: str
+    db_password: str
+    db_name: str
+
+    @property
+    def db_url(self) -> str:
+        return f"postgresql+asyncpg://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
+
+    @property
+    def db_url_sync(self) -> str:
+        return f"postgresql+psycopg2://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
+
+    model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
     )
